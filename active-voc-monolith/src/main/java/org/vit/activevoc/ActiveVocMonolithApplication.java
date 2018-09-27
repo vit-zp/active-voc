db100804c2fabe65bf12c7764b9b416f238806ab
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-09-27 10:18:56 +0300

    very first commit - app is up on local environment

---
+++ active-voc-monolith/src/main/java/org/vit/activevoc/ActiveVocMonolithApplication.java
@@ -0,0 +1,12 @@
+package org.vit.activevoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveVocMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveVocMonolithApplication.class, args);
+	}
+}
