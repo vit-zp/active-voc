b2f551277cea658f3fbd3d12f25214e198cde85b
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-09-27 11:13:16 +0300

    fixed test bypass security

--- active-voc-monolith/src/test/java/org/vit/activevoc/controller/index/IndexControllerTest.java
+++ active-voc-monolith/src/test/java/org/vit/activevoc/controller/index/IndexControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,6 +20,7 @@ public class IndexControllerTest {
 	private MockMvc mock;
 
 	@Test
+	@WithMockUser
 	public void shouldGetIndexPage() throws Exception {
 		mock.perform(MockMvcRequestBuilders.get("/"))
 			.andExpect(status().isOk())
